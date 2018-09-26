0a14103bc3f95c87f785f7dd1e9cba75d1d5bc98
alexmancheno <alex.mancheno@gmail.com>
2018-09-26 15:02:23 -0400

    Update

--- Misc/Solutions/Main04.java
+++ Misc/Solutions/Main04.java
@@ -6,7 +6,7 @@ public class Main04
     public static void main(String[] args) 
     {
         String[] wordArray = { "hello", "goodbye", "cat", "dog", "red", "green", "sun", "moon" };
-        String isOrIsNot, inputWord;
+        String isOrIsNot = null, inputWord = null;
 
         // This line asks the user for input by popping out a single window
         // with text input
@@ -21,9 +21,10 @@ public class Main04
                 isOrIsNot = "is"; // set to is if the word is on the list
             else
                 isOrIsNot = "is not"; // set to is not if the word is not on the list
+
+            // Output to a JOptionPane window whether the word is on the list or not
+            JOptionPane.showMessageDialog(null, "The word " + inputWord + " " + isOrIsNot + " on the list.");
         }
-        // Output to a JOptionPane window whether the word is on the list or not
-        JOptionPane.showMessageDialog(null, "The word " + inputWord + " " + isOrIsNot + " on the list.");
     }
 
     public static boolean wordIsThere(String findMe, String[] theList) 
